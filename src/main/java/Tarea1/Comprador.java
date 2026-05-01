f8483a84e5310736c8692e7d6b8def34dd353492
Tomas Pizarro <tpizarro2025@inf.udec.cl>
2026-05-01 12:45:06 -0400

    agregue el catch de pagoincorrecto al comprador

--- src/main/java/Tarea1/Comprador.java
+++ src/main/java/Tarea1/Comprador.java
@@ -5,18 +5,24 @@ public class Comprador {
     private int vuelto = 0;
 
     public Comprador(Moneda m, EnumProducto TipoProducto, Expendedor exp){
-        
         Producto p = null;
         try {
             p = exp.comprarProducto(m, TipoProducto);
+
             //Error en caso de acabarse el stock
         } catch (NoHayProductoException e) {
             System.out.println("Error: " + e.getMessage());
             p = null;
+
             //Error en caso de Pago insuficiente
         } catch (PagoInsuficienteException e){
             System.out.println("Error en el pago: " + e.getMessage());
             p = null;
+
+            //error en caso de moneda nula (pago incorrecto)
+        } catch (PagoIncorrectoException e){
+            System.out.println("Error de moneda: "+e.getMessage());
+            p =null;
         }
 
         if (p != null) {
